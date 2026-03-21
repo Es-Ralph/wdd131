531c71722a47c0dfcde7c8dce31af24951a7f6bc
Es-Ralph <ragyapong@byupathway.edu>
2026-03-21 19:57:58 +0000

    week03 project

--- week03/scripts/place.js
+++ week03/scripts/place.js
@@ -1,21 +1,22 @@
-// Footer
+// Footer Dynamic Values
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("modified").textContent = document.lastModified;
 
-// Static values
+// Static Weather Values
 const temp = 28;
 const wind = 10;
 
-// Function (ONE LINE RETURN — requirement!)
+// Wind Chill Function (ONE LINE RETURN)
 function calculateWindChill(t, v) {
     return (13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16)).toFixed(1);
 }
 
-// Condition check
+// Apply Conditions
 let chill = "N/A";
 
 if (temp <= 10 && wind > 4.8) {
     chill = calculateWindChill(temp, wind);
 }
 
+// Output Result
 document.getElementById("chill").textContent = chill;
